91d559127fd7c78263364b450b744e8d10cfe64d
anastasiosvanis <anastasiosvanis@gmail.com>
2021-03-08 09:25:25 +0200

    added new fields for user

--- models/user.py
+++ models/user.py
@@ -7,15 +7,33 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
+    age = db.Column(db.String(80))
+    height = db.Column(db.String(80))
+    weight = db.Column(db.String(80))
+    status = db.Column(db.String(80))
+    favSport = db.Column(db.String(80))
+    isAthlete = db.Column(db.Boolean)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,age, height, weight, status, favSport, isAthlete):
         self.username = username
         self.password = password
+        self.age = age
+        self.height = height
+        self.weight = weight
+        self.status = status
+        self.favSport = favSport
+        self.isAthlete = isAthlete
 
     def json(self):
         return {
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'age': self.age,
+            'height': self.height,
+            'weight': self.weight,
+            'status': self.status,
+            'favSport': self.favSport,
+            'isAthlete': self.isAthlete,
         }
 
     def save_to_db(self):
